de0e60969cc2c55b1c5b8e344b03cedb3725ea84
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-28 15:36:58 +0100

    Add standard tag values (may be temporary!)

--- newlib/newlib/libc/machine/riscv/sys/platform/tag.h
+++ newlib/newlib/libc/machine/riscv/sys/platform/tag.h
@@ -5,6 +5,19 @@
 
 #define TAG_WIDTH 4
 
+/* Standard tag values */
+
+/* No effect. */
+#define __RISCV_TAG_NONE 0
+/* Fault on write. */
+#define __RISCV_TAG_READ_ONLY 1
+/* Fault on read. */
+#define __RISCV_TAG_WRITE_ONLY 2
+/* Fault on read or write. */
+#define __RISCV_TAG_INVALID 3
+/* No effect, but cleared on write */
+#define __RISCV_TAG_LAZY 4
+
 int __riscv_load_tag(const void *addr);
 
 void __riscv_store_tag(void *addr, int tag);
