831b7b46d5a1a8826b859805c491a38ad9b0e27b
Matthew Toseland <mjt92@cam.ac.uk>
2015-07-30 14:14:46 +0100

    Expose tag-copying and no-tag-copying mem* to programmer

--- newlib/newlib/libc/machine/riscv/sys/platform/tag.h
+++ newlib/newlib/libc/machine/riscv/sys/platform/tag.h
@@ -18,6 +18,9 @@
 /* No effect, but cleared on write */
 #define __RISCV_TAG_LAZY 4
 
+/* Load tag, store tag. May be used in very low level code e.g. memcpy, so 
+ * important for it to be inline. */
+
 #ifndef INLINE
 # if __GNUC__ && !__GNUC_STDC_INLINE__
 #  define INLINE extern inline
@@ -42,5 +45,39 @@ INLINE void __riscv_store_tag(void *addr, unsigned char tag) {
                 :"r"(tag), "r"(addr)
                 );
 }
+
+/* Variants of memcpy and memmove with and without copying tags.
+ *
+ * SECURITY: You should only copy tags when there is a good reason to do so, as
+ * it may bypass tag-based protection (e.g. tagging of code pointers). You 
+ * should copy tags if copying structures including function pointers, but not 
+ * when copying strings, floats etc, unless adding your own tags.
+ *
+ * COMPATIBILITY: The default memcpy and memmove will copy tags if all three 
+ * arguments (including the length) are long-aligned. Hopefully compilers will
+ * do type-based analysis to improve performance and reduce the attack surface. 
+ */
+
+/* Copy length bytes from src to dst. Do not copy tags. aa and bb must not 
+ * overlap. Identical to a standard memcpy() without tagged memory support. 
+ * Should be used for strings, floats etc. */
+void* __riscv_memcpy_no_tags(void* dst, const void* src, size_t length);
+
+/* Copy length bytes from src to dst. Do not copy tags. aa and bb may overlap.
+ * Identical to a standard memmove() without tagged memory support.
+ * Should be used for strings, floats etc where overlap is possible. */
+void* __riscv_memmove_no_tags(void *dst, const void* src, size_t length);
+
+/* Copy length long's, including tags, from src to dst. dst and src must not
+ * overlap, and must be 64-bit aligned. Use this method for copying structures 
+ * that may contain code pointers etc. */
+unsigned long *__riscv_memcpy_tagged_longs(unsigned long *dst, 
+  const unsigned long* src, size_t length)
+
+/* Copy length long's, including tags, from src to dst. dst and src may 
+ * overlap, but must be 64-bit aligned. Use this method for copying structures 
+ * that may contain code pointers etc, where dst and src may overlap. */
+unsigned long *__riscv_memmove_tagged_longs(unsigned long *dst, 
+  const unsigned long* src, size_t length)
 
 #endif /* !LOWRISC_TAG_H */
