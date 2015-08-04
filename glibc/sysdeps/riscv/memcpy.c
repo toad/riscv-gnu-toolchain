c4010d396efc216f984176363c4a7c7dba7a335b
Matthew Toseland <mjt92@cam.ac.uk>
2015-08-04 16:52:43 +0100

    Use newlib version of memcpy.c
    I think this was the same previously

--- glibc/sysdeps/riscv/memcpy.c
+++ glibc/sysdeps/riscv/memcpy.c
@@ -1,10 +1,16 @@
 #include <string.h>
 #include <stdint.h>
-#include "tag.h"
+#include "sys/platform/tag.h"
 
 #define TAGGED_MEMORY
 
-void* __memcpy(void* aa, const void* bb, size_t n)
+#ifdef TAGGED_MEMORY
+#define MEMCPY_NAME __riscv_memcpy_no_tags
+#else
+#define MEMCPY_NAME memcpy
+#endif
+
+void* MEMCPY_NAME(void* aa, const void* bb, size_t n)
 {
   #define BODY(a, b, t) { \
     t tt = *b; \
@@ -12,23 +18,10 @@ void* __memcpy(void* aa, const void* bb, size_t n)
     *(a-1) = tt; \
   }
 
-#ifdef TAGGED_MEMORY
-  #define BODY_TAGGED(a, b) { \
-    long tt = *b; \
-    char tag = load_tag(b); \
-    a++, b++; \
-    *(a-1) = tt; \
-    store_tag(a-1, tag); \
-  }
-#endif
-
   char* a = (char*)aa;
   const char* b = (const char*)bb;
   char* end = a+n;
   uintptr_t msk = sizeof(long)-1;
-#ifdef TAGGED_MEMORY
-  int copyTags = !((uintptr_t)a & msk) && !((uintptr_t)b & msk) && !(n & msk);
-#endif
   if (__builtin_expect(((uintptr_t)a & msk) != ((uintptr_t)b & msk) || n < sizeof(long), 0))
   {
 small:
@@ -46,69 +39,32 @@ small:
   const long* lb = (const long*)b;
   long* lend = (long*)((uintptr_t)end & ~msk);
 
-#ifdef TAGGED_MEMORY
-  if(copyTags) {
-    if(__builtin_expect(la < lend-3, 0)) {
-      /* Only copy four at a time because will need regs for tags too.
-       * Compiler will use a whole reg for a char, right? */
-      while (la < lend-8)
-      {
-        long b0 = *lb++;
-        char t0 = load_tag(lb-1);
-        long b1 = *lb++;
-        char t1 = load_tag(lb-1);
-        long b2 = *lb++;
-        char t2 = load_tag(lb-1);
-        long b3 = *lb++;
-        char t3 = load_tag(lb-1);
-        *la++ = b0;
-        store_tag(la-1, t0);
-        *la++ = b1;
-        store_tag(la-1, t1);
-        *la++ = b2;
-        store_tag(la-1, t2);
-        *la++ = b3;
-        store_tag(la-1, t3);
-      }
+  if(__builtin_expect(la < lend-8, 0)) {
+    while (la < lend-8)
+    {
+      long b0 = *lb++;
+      long b1 = *lb++;
+      long b2 = *lb++;
+      long b3 = *lb++;
+      long b4 = *lb++;
+      long b5 = *lb++;
+      long b6 = *lb++;
+      long b7 = *lb++;
+      long b8 = *lb++;
+      *la++ = b0;
+      *la++ = b1;
+      *la++ = b2;
+      *la++ = b3;
+      *la++ = b4;
+      *la++ = b5;
+      *la++ = b6;
+      *la++ = b7;
+      *la++ = b8;
     }
-  } else {
-#endif
-    if(__builtin_expect(la < lend-8, 0)) {
-      while (la < lend-8)
-      {
-        long b0 = *lb++;
-        long b1 = *lb++;
-        long b2 = *lb++;
-        long b3 = *lb++;
-        long b4 = *lb++;
-        long b5 = *lb++;
-        long b6 = *lb++;
-        long b7 = *lb++;
-        long b8 = *lb++;
-        *la++ = b0;
-        *la++ = b1;
-        *la++ = b2;
-        *la++ = b3;
-        *la++ = b4;
-        *la++ = b5;
-        *la++ = b6;
-        *la++ = b7;
-        *la++ = b8;
-      }
-    }
-#ifdef TAGGED_MEMORY
   }
 
-  if(copyTags) {
-    while (la < lend)
-      BODY_TAGGED(la, lb);
-  } else {
-#endif
-    while (la < lend)
-      BODY(la, lb, long);
-#ifdef TAGGED_MEMORY
-  }
-#endif
+  while (la < lend)
+    BODY(la, lb, long);
 
   a = (char*)la;
   b = (const char*)lb;
@@ -116,5 +72,64 @@ small:
     goto small;
   return aa;
 }
-weak_alias (__memcpy, memcpy)
-libc_hidden_builtin_def (memcpy)
+
+#ifdef TAGGED_MEMORY
+
+/* All arguments must be long-aligned, and the length is in long's. */
+unsigned long* __riscv_memcpy_tagged_longs(unsigned long *la, 
+  const unsigned long* lb, size_t length) {
+
+  #define BODY_TAGGED(a, b) { \
+    long tt = *b; \
+    char tag = __riscv_load_tag(b); \
+    a++, b++; \
+    *(a-1) = tt; \
+    __riscv_store_tag(a-1, tag); \
+  }
+
+  unsigned long *ret = la;
+  unsigned long *lend = la + length;
+
+  if(__builtin_expect(la < lend-4, 0)) {
+    /* Only copy four at a time because will need regs for tags too.
+     * Compiler will use a whole reg for a char, right? */
+    while (la < lend-4)
+    {
+      long b0 = *lb++;
+      char t0 = __riscv_load_tag(lb-1);
+      long b1 = *lb++;
+      char t1 = __riscv_load_tag(lb-1);
+      long b2 = *lb++;
+      char t2 = __riscv_load_tag(lb-1);
+      long b3 = *lb++;
+      char t3 = __riscv_load_tag(lb-1);
+      *la++ = b0;
+      __riscv_store_tag(la-1, t0);
+      *la++ = b1;
+      __riscv_store_tag(la-1, t1);
+      *la++ = b2;
+      __riscv_store_tag(la-1, t2);
+      *la++ = b3;
+      __riscv_store_tag(la-1, t3);
+    }
+  }
+  while (la < lend)
+    BODY_TAGGED(la, lb);
+
+  return ret;
+}
+
+/* Nonzero if either X, Y or Z is not aligned on a "long" boundary.  */
+#define UNALIGNED3(X, Y, Z) \
+  (((long)X & (sizeof (long) - 1)) | ((long)Y & (sizeof (long) - 1)) | \
+  ((long)Z & (sizeof (long) - 1)))
+
+void* memcpy(void *dst, const void* src, size_t length) {
+  if(UNALIGNED3(dst, src, length))
+    return __riscv_memcpy_no_tags(dst, src, length);
+  else
+    return __riscv_memcpy_tagged_longs((unsigned long*)dst, 
+      (const unsigned long*)src, length/sizeof(unsigned long));
+}
+
+#endif /* TAGGED_MEMORY */
